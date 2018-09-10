4e482485b26f32d8c7e81657a5cb5b2ca2d49a3d
ristianandaaa <42725310+ristianandaaa@users.noreply.github.com>
2018-09-10 20:55:53 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,17 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
-?>
+	for($a = 1; $a <= $bin; $a++){
+		for($b = 1; $b <= $a; $b++){
+			echo "*";
+		}
+		echo "<br>";
+	}
+	?>
+===================================================
+<?php 
+      For ($i=1; $i<=10;$i++){  
+           For($j=1; $j<=$i;$j++){  
+                echo "";  
+           }  
+           echo '<br/>';  
+      }  
+ ?> 
